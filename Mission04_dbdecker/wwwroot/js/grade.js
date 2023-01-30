141d1f5e91e156b97f6cd53365e2c48f19d7a662
danielbdecker <danielbdecker@gmail.com>
2023-01-30 10:52:12 -0700

    Add project files.

---
+++ Mission04_dbdecker/wwwroot/js/grade.js
@@ -0,0 +1,32 @@
+﻿$("#btnSend").click(function () {
+    var grade = (($("#assignments").val()*0.5) + ($("#groupProject").val()*0.1) +
+        ($("#quizzes").val()*0.1) + ($("#midtermExam").val()*0.1) +
+        ($("#finalExam").val() * 0.1) + ($("#intex").val()) * 0.1);
+    var letterGrade
+    if (grade >= 94) {
+        letterGrade = "A";
+    } else if (grade >= 90) {
+        letterGrade = "A-";
+    } else if (grade >= 87) {
+        letterGrade = "B+";
+    } else if (grade >= 84) {
+        letterGrade = "B";
+    } else if (grade >= 80) {
+        letterGrade = "B";
+    } else if (grade >= 77) {
+        letterGrade = "C+";
+    } else if (grade >= 74) {
+        letterGrade = "C";
+    } else if (grade >= 70) {
+        letterGrade = "C-";
+    } else if (grade >= 67) {
+        letterGrade = "D+";
+    } else if (grade >= 64) {
+        letterGrade = "D";
+    } else if (grade >= 60) {
+        letterGrade = "D-";
+    } else {
+        letterGrade = "E";
+    }
+    $("#grade").text(`The percentage was ${grade}, which is a(n) ${letterGrade}.`);
+});
